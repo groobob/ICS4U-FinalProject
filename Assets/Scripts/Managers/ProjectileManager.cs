46a2ef87abafe0d10e7b865df00feeb5508d780f
Richard <elitegem84@gmail.com>
2024-01-19 21:14:05 -0500

    Added focus bar and health bar graphics into project

--- Assets/Scripts/Managers/ProjectileManager.cs
+++ Assets/Scripts/Managers/ProjectileManager.cs
@@ -23,7 +23,7 @@ public class ProjectileManager : MonoBehaviour
 
     public void SpawnProjectile(Vector2 position, Vector2 force, int projectileType)
     {
-        GameObject projectile = Instantiate(projectiles[projectileType], position, Quaternion.identity);
+        GameObject projectile = Instantiate(projectiles[projectileType], position, Quaternion.identity, transform);
         projectile.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
     }
 
@@ -32,7 +32,7 @@ public class ProjectileManager : MonoBehaviour
         float offset = projectileNum % 2 == 1 ? projectileNum / 2 * spreadInRadians * -1 : projectileNum / 2 * spreadInRadians * -1 + spreadInRadians / 2;
         for (int i = 0; i < projectileNum; i++)
         {
-            GameObject projectile = Instantiate(projectiles[projectileType], position, Quaternion.identity);
+            GameObject projectile = Instantiate(projectiles[projectileType], position, Quaternion.identity, transform);
             projectile.GetComponent<Rigidbody2D>().AddForce(rotateVector2(force, spreadInRadians * i + offset) * 45);
         }
     }
@@ -44,5 +44,14 @@ public class ProjectileManager : MonoBehaviour
             v.x * Mathf.Cos(radians) - v.y * Mathf.Sin(radians),
             v.x * Mathf.Sin(radians) + v.y * Mathf.Cos(radians)
         );
+    }
+
+    public void ClearAllProjectiles()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            var child = transform.GetChild(i).gameObject;
+            Destroy(child);
+        }
     }
 }
