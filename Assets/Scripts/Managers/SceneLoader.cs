7a94ca2ffd1ad15f3feaf8383f4854c65239d572
Richard <elitegem84@gmail.com>
2024-01-23 02:19:09 -0500

    Implemented cloud shooting and death scene transition

--- Assets/Scripts/Managers/SceneLoader.cs
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -22,6 +22,16 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
+    public void LoadEndScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadDeathScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
