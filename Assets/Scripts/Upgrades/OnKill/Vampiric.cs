70420c01f56ccde265d7946e2a3a1964d402f4de
Evan <revan879879@gmail.com>
2024-01-24 22:49:32 -0500

    yuyuyuf

--- Assets/Scripts/Upgrades/OnKill/Vampiric.cs
+++ Assets/Scripts/Upgrades/OnKill/Vampiric.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 public class Vampiric : OnKillUpgrades
 {
-    private int enemiesCount = 7;
+    private int enemiesCount = 5;
     private int current;
 
     public void Start()
@@ -20,7 +20,7 @@ public class Vampiric : OnKillUpgrades
         current = 0;
         base.Init();
         upgradeName = "Vampiric";
-        description = "Heal every " + enemiesCount + " enemies killed.";
+        description = "Heal every " + enemiesCount + " enemies killed by basic attacks.";
     }
 
     public override void attackEffect(Enemy e)
@@ -34,7 +34,7 @@ public class Vampiric : OnKillUpgrades
         {
             Destroy(Instantiate(PlayerManager.Instance.animations[5], e.transform.position, Quaternion.identity), 0.533f);
             current = 0;
-            SoundManager.Instance.PlayAudio(9);
+            SoundManager.Instance.PlayAudio(17);
             _playerStats.HealDamage(1);
         }
     }
